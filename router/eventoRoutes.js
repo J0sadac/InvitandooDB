de912056c493b9aa5b5953749717c3fc0b847dcc
Josadac Castaneda <jugdmentc12@gmail.com>
2023-11-05 14:35:10 -0600

    API lista de nuevo

--- router/eventoRoutes.js
+++ router/eventoRoutes.js
@@ -7,7 +7,7 @@ async function obtenerEvento(req, res, next) {
     try {
         evento = await Evento.findById(req.params.id);
         if (evento == null) {
-            return res.status(404).json({ mensaje: 'No se porque no aparece' });
+            return res.status(404).json({ mensaje: 'Evento no econtrado.' });
         }
     } catch (error) {
         return res.status(500).json({ mensaje: error.message });
@@ -17,24 +17,48 @@ async function obtenerEvento(req, res, next) {
     next();
 }
 
+
 // GET
-// Ruta para obtener todos los eventos y sus invitados
-// Ruta para obtener todos los eventos y sus invitados
+// Ruta para obtener un invitado por su ID para un anfitrión específico o el evento completo si no se proporciona ningún parámetro
 router.get('/', async (req, res) => {
-    const { anfitrion } = req.query;
+    const anfitrionParam = req.query.anfitrion; // Obtiene el valor del parámetro 'anfitrion' de la URL
+    const invitadoIdParam = req.query.invitadoId; // Obtiene el valor del parámetro 'invitadoId' de la URL
 
     try {
-        let eventos;
-        if (anfitrion) {
-            eventos = await Evento.find({ anfitrion: anfitrion });
+        if (!anfitrionParam && !invitadoIdParam) {
+            // Si no se proporcionan ambos parámetros, devuelve el evento completo
+            const eventos = await Evento.find();
+            res.json(eventos);
         } else {
-            eventos = await Evento.find();
+            // Si se proporcionan ambos parámetros, busca el evento del anfitrión específico
+            let evento = await Evento.findOne({ anfitrion: anfitrionParam });
+
+            if (evento) {
+                if (invitadoIdParam) {
+                    // Si se proporciona el ID del invitado, busca y devuelve solo ese invitado del evento
+                    const invitado = evento.invitados.find(i => i._id === invitadoIdParam);
+                    if (invitado) {
+                        const eventoConInvitado = {
+                            ...evento.toObject(),
+                            invitados: [invitado]
+                        };
+                        res.json(eventoConInvitado);
+                    } else {
+                        res.status(404).json({ mensaje: 'Invitado no encontrado.' });
+                    }
+                } else {
+                    // Si no se proporciona el ID del invitado, devuelve el evento completo
+                    res.json(evento);
+                }
+            } else {
+                res.status(404).json({ mensaje: 'Evento no encontrado.' });
+            }
         }
-        res.json(eventos);
     } catch (error) {
         res.status(500).json({ mensaje: error.message });
     }
 });
+
 
 
 router.get('/:id', obtenerEvento, (req, res) => {
